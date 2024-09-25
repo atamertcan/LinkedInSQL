b524ae456c74ef453bb42c8ff7955ba636ccbc7d
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-25 16:28:45 +0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,8 @@
-
+CREATE TABLE "users" (
+    "id" INTEGER,
+    "first_name" TEXT NOT NULL,
+    "last_name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+);
