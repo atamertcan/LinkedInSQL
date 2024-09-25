68eb81acecb3ced37711949fa7d5ad05470a7e1d
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-25 16:29:08 +0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -16,3 +16,11 @@ CREATE TABLE "schools_and_universities" (
     PRIMARY KEY ("id")
 
 );
+
+CREATE TABLE "companies" (
+    "id" INTEGER,
+    "name" TEXT NOT NULL,
+    "industry" TEXT NOT NULL,
+    "location" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+);
