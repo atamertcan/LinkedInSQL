63e38a48f120b83287d7bbfe90e9b46e25d2c40a
atamertcan <142722122+atamertcan@users.noreply.github.com>
2024-09-25 16:30:11 +0300

    schema.sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,10 @@
+-- Users
+-- The heart of LinkedIn’s platform is its people.
+-- Your database should be able to represent the following information about LinkedIn’s users:
+--     * Their first and last name
+--     * Their username
+--     * Their password
+
 CREATE TABLE "users" (
     "id" INTEGER,
     "first_name" TEXT NOT NULL,
